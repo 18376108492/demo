5d97eb3b8a5151fb2700093054f689386feadd5c
dan <2207161187@qq.com>
2020-09-28 21:56:19 +0800

    init

---
+++ nacos_consumer/src/main/java/com/itdan/nacos/consumer/SpringRestConsumerBootstrap.java
@@ -0,0 +1,12 @@
+package com.itdan.nacos.consumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class SpringRestConsumerBootstrap {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringRestConsumerBootstrap.class,args);
+    }
+}
